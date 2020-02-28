de4a6906319b56ff4fd6d9b59fb8b6a40f24397b
Endre <endre.bruaset@gmail.com>
2020-02-28 15:15:12 +0000

    finalized doxygen documentation

--- source/fsm.h
+++ source/fsm.h
@@ -1,15 +1,12 @@
 /**
  * \mainpage Elevator Project - TTK4235
- *      \section Introduction
- * Documentation of an implementation of a control system controlling an elevator, as a project in the course TTK4235. 
- * The documentation includes an overview of the different modules, and the public APIs used.
  * 
- *      \section About the Architecture Design
- * The elevator control system is implemented by modeling the elevator as a Finite State Machine (FSM). The interface to
- * the elevator hardware was pre-made, and is documented in @c harware.h. The remaining necessary functionalities were 
- * added through the queue and timer modules.
+ * Documentation of implementation of a control system controlling an elevator, as a project in the course TTK4235. 
+ * The documentation includes an overview of the different modules, and the public APIs used. The elevator control system 
+ * is implemented by modeling the elevator as a Finite State Machine (FSM). The interface to the elevator hardware was pre-made, 
+ * and is documented in @c hardware.h. The remaining necessary functionalities were added through the queue and timer modules.
  * 
- *      \authors Endre Bruaset and Nina Nyegaarden
+ * By Endre Bruaset and Nina Nyegaarden
  */
 
 
